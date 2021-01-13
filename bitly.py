41ea106a1dd7b181fb453fca0af01e6d5c95689a
Aleksandr Semenov <semenlinux@gmail.com>
2021-01-13 19:45:10 +0300

    Initial commit

---
+++ bitly.py
@@ -0,0 +1,10 @@
+import requests
+
+import settings
+
+def request_handler(url, token):
+    request_data = requ
+
+if __name__=="__main__":
+    
+    print(settings.TOKEN)
---
+++ settings.py
@@ -0,0 +1,2 @@
+TOKEN="8bb2d4597772e92e404c2d20329449ad709af107"
+URL="https://api-ssl.bitly.com/v4/user"

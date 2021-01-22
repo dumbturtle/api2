0b18c89fdb74840a14c29a9dfdcb89314898bc44
Aleksandr Semenov <semenlinux@gmail.com>
2021-01-22 22:05:14 +0300

    Add support other domain

--- bitly.py
+++ bitly.py
@@ -14,58 +14,69 @@ def createParser():
         "asked_url",
         help="Укажите ссылку для уменьшения или адрес для проверки кол-ва переходов. Пример: https://google.com",
     )
-
     return parser
+
+
+def convert_bitlink(link):
+    link_https = "https"
+    if link.startswith(link_https):
+        link = f"{urlparse(link).netloc}{urlparse(link).path}"
+    return link
+
+
+def is_bitlink(link, token):
+    link = convert_bitlink(link)
+    headers = {"Authorization": f"Bearer { token }"}
+    bit_link = f"https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks"
+    response_data = requests.get(bit_link, headers=headers)
+    return response_data.ok
 
 
 def shorten_link(url, token):
     headers = {"Authorization": f"Bearer { token }"}
-    bilty_url = {"long_url": url}
+    bitly_url = {"long_url": url}
     response_data = requests.post(
-        os.getenv("LINK_URL"), headers=headers, json=bilty_url
+        os.getenv("LINK_URL"), headers=headers, json=bitly_url
     )
     response_data.raise_for_status()
-    bilty_link = response_data.json().get("link")
-    return bilty_link
+    bitly_link = response_data.json().get("link")
+    return bitly_link
 
 
-def count_clicks(link, token):
+def count_clicks_link(link, token):
     headers = {"Authorization": f"Bearer { token }"}
-    bitlink_url = f"https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks"
+    link = convert_bitlink(link)
+    bit_link = f"https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks"
     params = {
         "unit": "day",
         "units": 1,
     }
-    response_data = requests.get(bitlink_url, headers=headers, params=params)
+    response_data = requests.get(bit_link, headers=headers, params=params)
     response_data.raise_for_status()
     clicks_count = response_data.json().get("link_clicks")[0].get("clicks")
     return clicks_count
 
 
 def handler_url(url):
-    bilty_url = "bit.ly"
-    bilty_url_https = "https://bit.ly"
-    if url.startswith(bilty_url_https):
-        url = f"{urlparse(url).netloc}{urlparse(url).path}"
-    if url.startswith(bilty_url):
-        try:
-            clicks = f"Количество переходов: {count_clicks(url, os.getenv('TOKEN'))}"
-        except requests.exceptions.HTTPError:
-            clicks = os.getenv("ERROR_TEXT")
-        return clicks
     try:
+        if is_bitlink(url, os.getenv("TOKEN")):
+            clicks = (
+                f"Количество переходов: {count_clicks_link(url, os.getenv('TOKEN'))}"
+            )
+            return clicks
         short_url = f"Краткая ссылка: {shorten_link(url, os.getenv('TOKEN'))}"
+        return short_url
     except requests.exceptions.HTTPError:
-        short_url = os.getenv("ERROR_TEXT")
-    return short_url
+        return os.getenv("ERROR_TEXT")
+
 
 def main():
     load_dotenv()
     parser = createParser()
     namespace = parser.parse_args()
-    bilty_url = namespace.asked_url
-    print(handler_url(bilty_url))
+    bitly_url = namespace.asked_url
+    print(handler_url(bitly_url))
+
 
 if __name__ == "__main__":
     main()
-

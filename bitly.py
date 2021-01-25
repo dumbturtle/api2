6e91104f06b1f1fd9af75dcbff921b6c86f42c85
Aleksandr Semenov <semenlinux@gmail.com>
2021-01-25 15:10:30 +0300

    Use simple nouns as variable names

--- bitly.py
+++ bitly.py
@@ -18,51 +18,49 @@ def create_commandline_parser():
 
 
 def cut_bitlink(link):
-    link_https = "https"
-    if link.startswith(link_https):
+    https_link = "https"
+    if link.startswith(https_link):
         link = f"{urlparse(link).netloc}{urlparse(link).path}"
     return link
 
 
-def is_bitlink(link, token):    
+def is_bitlink(link, token):
     link = cut_bitlink(link)
     headers = {"Authorization": f"Bearer { token }"}
     bit_link = f"https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks"
-    response_bitly = requests.get(bit_link, headers=headers)
-    return response_bitly.ok
+    bitly_api_response = requests.get(bit_link, headers=headers)
+    return bitly_api_response.ok
 
 
 def create_short_link(url, api_bitly, token):
     headers = {"Authorization": f"Bearer { token }"}
     bitly_url = {"long_url": url}
-    response_bitly = requests.post(api_bitly, headers=headers, json=bitly_url)
-    response_bitly.raise_for_status()
-    bitly_link = response_bitly.json().get("link")
-    return bitly_link
+    bitly_api_response = requests.post(api_bitly, headers=headers, json=bitly_url)
+    bitly_api_response.raise_for_status()
+    bitly_short_link = bitly_api_response.json().get("link")
+    return bitly_short_link
 
 
-def count_clicks_link(link, token):
+def count_link_clicks(link, token):
     headers = {"Authorization": f"Bearer { token }"}
-    sum_clicks = 0
-    link = cut_bitlink(link)
-    bitly_api_click_url = f"https://api-ssl.bitly.com/v4/bitlinks/{link}/clicks"
-    response_bitly = requests.get(bitly_api_click_url, headers=headers)
-    response_bitly.raise_for_status()
-    clicks = response_bitly.json().get("link_clicks")
-    for click in clicks:
-        sum_clicks = sum_clicks + click.get("clicks", 0)
-    return sum_clicks
+    bitly_sum_clicks = 0
+    user_link = cut_bitlink(link)
+    bitly_api_click_url = f"https://api-ssl.bitly.com/v4/bitlinks/{user_link}/clicks"
+    bitly_api_response = requests.get(bitly_api_click_url, headers=headers)
+    bitly_api_response.raise_for_status()
+    bitly_clicks = bitly_api_response.json().get("link_clicks")
+    for click in bitly_clicks:
+        bitly_sum_clicks = bitly_sum_clicks + click.get("clicks", 0)
+    return bitly_sum_clicks
 
 
 def process_query_link(link, token, api_bitly, message_error):
     try:
         if is_bitlink(link, token):
-            count_clicks = count_clicks_link(link, token)
-            clicks = f"Количество переходов: { count_clicks }"
-            return clicks
+            quantity_link_clicks = count_link_clicks(link, token)
+            return f"Количество переходов: { quantity_link_clicks }"
         short_link = create_short_link(link, api_bitly, token)
-        bitly_link = f"Краткая ссылка: { short_link }"
-        return bitly_link
+        return f"Краткая ссылка: { short_link }"
     except requests.exceptions.HTTPError:
         return message_error
 
@@ -71,11 +69,11 @@ def main():
     load_dotenv()
     bitly_api_token = os.getenv("BITLY_API_TOKEN")
     message_error = os.getenv("ERROR_TEXT")
-    api_bitly = os.getenv("LINK_URL")
+    bitly_api = os.getenv("LINK_URL")
     commandline_parser = create_commandline_parser()
     user_arguments = commandline_parser.parse_args()
-    bitly_link = user_arguments.asked_url
-    message = process_query_link(bitly_link, bitly_api_token, api_bitly, message_error)
+    user_link = user_arguments.asked_url
+    message = process_query_link(user_link, bitly_api_token, bitly_api, message_error)
     print(message)
 
 
